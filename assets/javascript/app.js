994ea404688b0893e1879ce0f82020ec684648e3
Tali Ngatuvai <tali_ngatuvai@talis-mbp.uconnect.utah.edu>
2019-06-29 12:10:11 -0600

    added moment.min CDN, and the correct j query CDN

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,32 @@
-$(document).ready(function(){
-    
-    var firebaseConfig = {
+$(document).ready(function () {
+
+    const firebaseConfig = {
         apiKey: "AIzaSyDqXLjKny8plL8ED0m2K91WxbzCvSNQAD4",
         authDomain: "train-scheduler-79f46.firebaseapp.com",
         databaseURL: "https://train-scheduler-79f46.firebaseio.com",
         projectId: "train-scheduler-79f46",
-        storageBucket: "",
+        storageBucket: "train-scheduler-79f46.appspot.com",
         messagingSenderId: "519140144729",
         appId: "1:519140144729:web:e5b7d13cae579362"
-      };
+    };
 
-      firebase.initializeApp(firebaseConfig);
-
+    firebase.initializeApp(firebaseConfig);
+    console.log("firebase", firebase);
     var database = firebase.database();
+    console.log(database);
     var name;
     var destination;
     var firstTrain;
     var frequency = 0;
 
-    $("#add-train").on("click", function() {
+    $("#add-train").on("click", function () {
         event.preventDefault();
         name = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
         firstTrain = $("#first-train").val().trim();
         frequency = $("#frequency").val().trim();
 
-        
+
         database.ref().push({
             name: name,
             destination: destination,
@@ -36,7 +37,7 @@ $(document).ready(function(){
         $("form")[0].reset();
     });
 
-    database.ref().on("child_added", function(childSnapshot) {
+    database.ref().on("child_added", function (childSnapshot) {
         var nextArr;
         var minAway;
         var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
@@ -47,18 +48,18 @@ $(document).ready(function(){
         nextTrain = moment(nextTrain).format("hh:mm");
 
         $("#add-row").append("<tr><td>" + childSnapshot.val().name +
-                "</td><td>" + childSnapshot.val().destination +
-                "</td><td>" + childSnapshot.val().frequency +
-                "</td><td>" + nextTrain + 
-                "</td><td>" + minAway + "</td></tr>");
+            "</td><td>" + childSnapshot.val().destination +
+            "</td><td>" + childSnapshot.val().frequency +
+            "</td><td>" + nextTrain +
+            "</td><td>" + minAway + "</td></tr>");
 
-        
-        }, function(errorObject) {
-            console.log("Errors handled: " + errorObject.code);
+
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
 
-    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-        
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
+
         $("#name-display").html(snapshot.val().name);
         $("#email-display").html(snapshot.val().email);
         $("#age-display").html(snapshot.val().age);
